16e095e1acacf38b7a17387ba496f655bce72fef
XY <3400164864@qq.com>
2023-11-12 09:25:56 +0800

    docs: 修改swagger文档

--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -1,18 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class SignInDto {
+  @ApiProperty()
   userEmail: string;
+  @ApiProperty()
   password: string;
 }
 
 export class SignUpDto {
+  @ApiProperty()
   userEmail: string;
+  @ApiProperty()
   code: number;
+  @ApiProperty()
   password: string;
 }
 
 export class EditPasswordDto {
+  @ApiProperty()
   userEmail: string;
+  @ApiProperty()
   code: number;
+  @ApiProperty()
   password: string;
 }
