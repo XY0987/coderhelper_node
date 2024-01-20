ef5d1e2725129fb92f432a432853682e324567e4
Zzhgitup <99540215+Zzhgitup@users.noreply.github.com>
2024-01-20 10:56:51 +0800

    🐞 fix: Proxy 代理优化

--- src/Proxymiddle/Proxy.controller.ts
+++ src/Proxymiddle/Proxy.controller.ts
@@ -1,17 +1,27 @@
 import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('dynamic-proxy')
+@ApiTags('代理')
 export class ProxyController {
   @Get()
+  @ApiOperation({ summary: 'get代理' })
   getIndex() {
     return '你好';
   }
   @Post()
+  @ApiOperation({ summary: 'Post代理' })
   getPost() {
     return '你好';
   }
   @Put()
-  getPut() {}
+  @ApiOperation({ summary: 'Put代理' })
+  getPut() {
+    return '你好';
+  }
   @Delete()
-  getdelete() {}
+  @ApiOperation({ summary: 'delete代理' })
+  getdelete() {
+    return '你好';
+  }
 }
--- src/Proxymiddle/dynamic-proxy-middleware.ts
+++ src/Proxymiddle/dynamic-proxy-middleware.ts
@@ -1,20 +1,51 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { createProxyMiddleware, Options } from 'http-proxy-middleware';
 import { NextFunction, Request, Response } from 'express';
+import { ClientRequest } from 'http';
+import * as multer from 'multer';
 //代理中间件
 @Injectable()
 export class DynamicProxyMiddleware implements NestMiddleware {
+  private upload = multer();
   use(req: Request, res: Response, next: NextFunction) {
+    console.log('Incoming request:', req.method, req.url);
     const targetUrl = req.query.targetUrl || 'http://localhost:8080';
     // 从请求路径中移除 '/dynamic-proxy' 部分
     const targetPath = req.url.replace(/^\/dynamic-proxy/, '');
-
-    // 创建代理中间件，并设置动态获取的代理地址和路径
     const proxyMiddleware = createProxyMiddleware({
       target: targetUrl,
       changeOrigin: true,
       pathRewrite: {
         '^/dynamic-proxy': '', // 移除 '/dynamic-proxy' 部分
+      },
+      methods: ['GET', 'POST', 'DELETE', 'PUT'], // 指定支持的请求方法
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      //在转发之前，对body数据进行处理
+      onProxyReq: (proxyReq: ClientRequest, req: Request) => {
+        const contentType = req.headers['content-type'];
+        if (contentType) {
+          if (contentType.includes('multipart/form-data')) {
+            console.log('发送文件数据');
+            this.upload.none()(req, res, (err: any) => {
+              if (!err && req.body) {
+                const bodyData = JSON.stringify(req.body);
+                proxyReq.setHeader('Content-Type', 'application/json');
+                proxyReq.setHeader(
+                  'Content-Length',
+                  Buffer.byteLength(bodyData),
+                );
+                proxyReq.write(bodyData);
+              }
+            });
+          } else {
+            console.log('发送JSON数据');
+            // 处理JSON数据
+            const bodyData = JSON.stringify(req.body);
+            proxyReq.setHeader('Content-Type', 'application/json');
+            proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+            proxyReq.write(bodyData);
+          }
+        }
       },
     } as Options);
     // 修改请求路径
